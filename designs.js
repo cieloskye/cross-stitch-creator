69237e77e893b2e7e1d6f8a59683f45f2ec22de9
cieloskye <skyeldemoya@gmail.com>
2019-01-30 21:22:16 -0800

    Updated submit button function to call makeGrid() and collect height & width information.

--- designs.js
+++ designs.js
@@ -3,20 +3,20 @@
 // When size is submitted by the user, call makeGrid()
 
 
-const height = $('#inputHeight');
-const weight = $('#inputWeight');
+
 const colorPicker = $('#colorPicker');
 const pixelCanvas = $('#pixelCanvas');
 const submit = $('input[type="submit"]');
 
-function makeGrid() {
+function makeGrid(height, width) {
 
-
+console.log('makeGrid, make that grid!');
  /* N by M
-  Use JavaScript loops to dynamically clear and create the table based on user input. Each cell should have an event listener that sets the background color of the cell to the selected color.
+  Use JavaScript loops to dynamically clear and create the 
+  table based on user input. 
+  Each cell should have an event listener that sets the 
+  background color of the cell to the selected color.
    */
-
-
 };
 
 //button
@@ -24,4 +24,9 @@ submit.on('click', function(event) {
 
 	event.preventDefault();
 	console.log('submit listen test');
+
+	const height = $('#inputHeight').val();
+	const weight = $('#inputWeight').val();
+
+	makeGrid();
 });
