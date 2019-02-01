f5173dcd0490a84952b8088b716394c500800a36
cieloskye <skyeldemoya@gmail.com>
2019-01-31 23:50:36 -0800

    Starting on makeGrid function.  Haven't cracked it just yet.

--- designs.js
+++ designs.js
@@ -11,16 +11,27 @@ const submit = $('input[type="submit"]');
 function makeGrid(height, width) {
 
 console.log('makeGrid, make that grid!');
- /* N by M
+ /* N by M tr & td
   Use JavaScript loops to dynamically clear and create the 
   table based on user input. 
   Each cell should have an event listener that sets the 
   background color of the cell to the selected color.
    */
+ 
+ pixelCanvas.on.('click', td, () => {
+
+
+ 	$(this).css('background-color', colorPicker.val()0;
+ });
+
+
+    //append($('<tr></tr>'));
+
+	//for while
 };
 
 //button
-submit.on('click', function(event) {
+submit.on('click', (event) => {
 
 	event.preventDefault();
 	console.log('submit listen test');
