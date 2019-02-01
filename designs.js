10e7ccf62d4d170f3b205888d39f09896c3c1e26
cieloskye <skyeldemoya@gmail.com>
2019-02-01 03:27:04 -0800

    Whoops my loops had commas instead of semicolons.  Code isn't throwing any more errors but the only thing that is happening is console.logs.

--- designs.js
+++ designs.js
@@ -1,41 +1,37 @@
 
-
+//const submit = ;
 
 //This will create the canvas once user input is received.
 function makeGrid(height, width) {
 	const colorPicker = $('#colorPicker');
 	const pixelCanvas = $('#pixelCanvas');
 
+	
+
+
 	console.log('makeGrid, make that grid!');
 
- //Clears Canvas
- pixelCanvas.innerHTML = '';
+ 	//Clears Canvas
+ 	pixelCanvas.innerHTML = '';
 
-//Creates canvas
-for(let n = 0, n < gridHeight, n++) {
-	pixelCanvas.append($('<tr></tr>'));
-}
+	//Creates canvas
+	for(let n = 0; n < height; n++) {
+		pixelCanvas.append($('<tr></tr>'));
+	};
 
-for(let m = 0, m < gridWidth, m++){
-	$('tr').last().append($('<td></td>'));
-}
+	for(let m = 0; m < width; m++){
+		$('tr').last().append($('<td></td>'));
+	};
 
 //Handles pixel color
-pixelCanvas.on.('click', td, () => {
-
-
- 	$(this).css('background-color', colorPicker.val()0;
- });
-
-
-    //append($('<tr></tr>'));
-
-	//for while
+//pixelCanvas.on.('click', td, () => {
+// 	$(this).css('background-color', colorPicker.val()0;
+// });
 };
 
 //Handle the submit button click, and call makeGrid().
-submit.on('click', (event) => {
-	const submit = $('input[type="submit"]');
+$('input[type="submit"]').on('click', (event) => {
+	
 	event.preventDefault();
 	console.log('submit listen test');
 
